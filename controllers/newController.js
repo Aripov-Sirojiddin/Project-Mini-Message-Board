3930bc11bb7fad8b51811fef2509111deb259544
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-21 17:51:34 +0500

    Form now has error checking for missing properties.

--- controllers/newController.js
+++ controllers/newController.js
@@ -3,18 +3,38 @@ const { addMessage } = require("../models/messages");
 async function getForm(req, res) {
   const locals = {
     title: "New message",
+    errors: [],
+    values: {},
   };
 
   res.render("pages/form.ejs", locals);
 }
 
 async function createNewMessage(req, res) {
+  const errors = [];
   const newMessage = {
     ...req.body,
     added: new Date(),
   };
-  addMessage(newMessage);
-  res.redirect("/");
+
+  console.log(req.body);
+  if (newMessage.user == null || newMessage.user === "") {
+    errors.push("Name can not be empty");
+  }
+  if (newMessage.text == null || newMessage.text === "") {
+    errors.push("Text can not be empty");
+  }
+
+  if (errors.length === 0) {
+    addMessage(newMessage);
+    res.redirect("/");
+  } else {
+    res.render("pages/form.ejs", {
+      title: "New message",
+      errors: errors,
+      values: newMessage,
+    });
+  }
 }
 
 module.exports = {
