36e55f1520b43ccf99e9b101a8034e9ce570e0f9
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-21 16:08:32 +0500

    Updated the date formating

--- helpers/formatDate.js
+++ helpers/formatDate.js
@@ -22,9 +22,19 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  return `${date.getHours()}:${date.getMinutes()} on ${days[date.getDay()]} ${
+
+  let hours = date.getHours();
+  hours = hours < 10 ? `0${hours}` : hours;
+
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  let monthDay = date.getDate();
+  monthDay = monthDay < 10 ? `0${monthDay}` : monthDay;
+
+  return `${hours}:${minutes} on ${days[date.getDay()]} ${
     months[date.getMonth()]
-  } ${date.getDate()}, ${date.getFullYear()}`;
+  } ${monthDay}, ${date.getFullYear()}`;
 }
 
 module.exports = formatDate;
