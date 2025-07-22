953bacf3da717b67373e722378cc6185025f59d1
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-07-22 15:26:26 +0500

    Added new fields to the models.

--- models/messages.js
+++ models/messages.js
@@ -2,12 +2,24 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
+    email: "amando@example.com",
     added: new Date(),
+    age: 32,
   },
   {
     text: "Hello World!",
     user: "Charles",
+    email: "charles@example.com",
     added: new Date(),
+    bio: "This is my amazing bio.",
+  },
+  {
+    text: "How are yall!",
+    user: "Mishel",
+    email: "mishel@example.com",
+    added: new Date(),
+    age: 36,
+    bio: "This is my amazing bio 2.",
   },
 ];
 
